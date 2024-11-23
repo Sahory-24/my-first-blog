2b2dda10428ac0f699cef20767015720cf6938a6
Sahory-24 <sahoryblanco24@gmail.com>
2024-11-23 16:25:26 -0500

    host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n(g1xqpc6a(nbnb3i20nbxycn(xc@4j0pxr-p5v&16qv3wg-53
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['sahory24.pythonanywhere.com']
+ALLOWED_HOSTS =['127.0.0.1:8000','sahory24.pythonanywhere.com']
 
 
 # Application definition
